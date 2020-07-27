f213ca6c79f776b8ec11d1012d4b85dbbcc53f35
MaximTimokhin <ffoka96@icloud.com>
2020-07-27 17:27:11 +0300

    Initial Commit

---
+++ L4_MaksimTimokhin(HW)/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L4_MaksimTimokhin(HW)
+//
+//  Created by Максим Тимохин on 27.07.2020.
+//  Copyright © 2020 Максим Тимохин. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
